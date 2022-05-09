db650638a6eda90d720344d2421a4db196c69f3c
A-1Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-05-09 08:10:18 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -1,36 +1,39 @@
-<article>
 (function() {
   function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
+    
+  <article>
 
-    if (results.length) { // Are there any results?
-      var appendString = '';
+      if (results.length) { // Are there any results?
+        var appendString = '';
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + ' ....</p></li>';
-      }
+        for (var i = 0; i < results.length; i++) {  // Iterate over the results
+          var item = store[results[i].ref];
+          appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+          appendString += '<p>' + item.content.substring(0, 150) + ' ....</p></li>';
+        }
 
-      searchResults.innerHTML = appendString;
-    } else {
-      searchResults.innerHTML = '<li>Maaf, tidak ada ;(</li>';
-    }
-  }
-
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        searchResults.innerHTML = appendString;
+      } else {
+        searchResults.innerHTML = '<li>Di sini tidak garap yang Anda cari, ya .... ;(</li>';
       }
     }
-  }
 
+    function getQueryVariable(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split('&');
+
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+
+        if (pair[0] === variable) {
+          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        }
+      }
+    }
+
+  </article>
+  
   var searchTerm = getQueryVariable('query');
 
   if (searchTerm) {
@@ -60,4 +63,3 @@
     }
   }
 })();
-</article>
