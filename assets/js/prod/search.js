0fa52d17d514c938309883ce0d02e97623cdee40
A-1Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-05-28 18:10:35 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -8,6 +8,8 @@
         for (var i = 0; i < results.length; i++) {  // Iterate over the results
           var item = store[results[i].ref];
           appendString += '<li><a href="' + item.url + '"><h4>' + item.title + '</h4></a>';
+          appendString += '<a href="' + item.url + '" class="ar-container">' + item.coverPhoto + '</a>'
+            <span class="ar-content" style="background-image: url({{ item.coverPhoto }});"></span>;
           appendString += '<p><h6>' + item.content.substring(0, 150) + ' ....</h6></p></li>';
         }
 
