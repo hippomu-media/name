9a4bc5a14d8329e3aae385f5ce64ee5f927f5515
A-1Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-05-29 07:16:01 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -30,12 +30,6 @@
       }
     }
   
-  array.sort(function(a,b){
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
-  return new Date(b.date) - new Date(a.date);
-});
-  
   var searchTerm = getQueryVariable('query');
 
   if (searchTerm) {
