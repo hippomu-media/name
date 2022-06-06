0cd0d807d9afa9193e888f2667704dafb0937b8c
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-06 19:26:30 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -7,7 +7,7 @@
         
         for (var i = 0; i < results.length; i++) {  // Iterate over the results
           var item = store[results[i].ref];
-          appendString += '<h4><a href="' + item.url + '" class="h4">' + item.title + '</a></h4>';
+          appendString += '<li><a href="' + item.url + '" class="h4">' + item.title + '</a></li>';
         }
 
         searchResults.innerHTML = appendString;
