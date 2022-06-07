348533fc464222861e9847a4e1f1e68f78709527
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-07 23:08:36 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -37,12 +37,20 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
+      this.field('id');
       this.field('title', { boost: 10 });
+      this.field('author');
+      this.field('category');
+      this.field('content');
     });
 
     for (var key in window.store) { // Add the data to lunr
       idx.add({
+        'id': key,
         'title': window.store[key].title,
+        'author': window.store[key].author,
+        'category': window.store[key].category,
+        'content': window.store[key].content
       });
 
       var results = idx.search(searchTerm); // Get lunr to perform a search
