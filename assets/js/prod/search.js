8197170ffc224f210b55becb41ed85d227d38354
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-28 18:17:39 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -37,8 +37,8 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
-      this.field('title');
-      this.field('date', { boost: 10 });
+      this.field('title', { boost: 10 });
+      this.field('date', { boost: 20 });
       this.field('id');
       this.field('author');
       this.field('category');
