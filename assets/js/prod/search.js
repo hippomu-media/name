f7899826a8c8f4ba718a25f79ff072821119bb3f
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-29 09:29:26 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -38,7 +38,7 @@
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('id');
-      this.date('date');
+      this.field('date');
     });
 
     for (var key in window.store) { // Add the data to lunr
