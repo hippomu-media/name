fb8dfc708b8f1687712b85598dc15f0330b9096e
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-29 08:45:09 +0700

    Update search.js

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -42,6 +42,7 @@
       this.field('id');
       this.field('author');
       this.field('category');
+      this.field('content');
     });
 
     for (var key in window.store) { // Add the data to lunr
@@ -50,7 +51,8 @@
         'eps': window.store[key].eps,
         'id': key,
         'author': window.store[key].author,
-        'category': window.store[key].category
+        'category': window.store[key].category,
+        'content': window.store[key].content,
       });
 
       var results = idx.search(searchTerm); // Get lunr to perform a search
